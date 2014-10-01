26e527da829a80b183df15d598a0f87c55f5b51c
Prashant <prathod@tavisca.com>
2014-10-01 11:21:24 +0530

    Changed the implementation of the selected date color change

--- starter-kit-1.7.0/js/app.js
+++ starter-kit-1.7.0/js/app.js
@@ -8,26 +8,39 @@ App.Router.map(function(){
 
 App.DatePickerView = Em.View.extend({
 	tagName : 'input',
-	didInsertElement : function (){
-		var options = {
-			multidate : true
-		};
-		this.$().datepicker(options);
-		var self = this;
-		this.$().datepicker().on('changeDate',function(e){
+	self : null,
+	selectedDates : null,
+	startDate : null,
+	endDate : null,
+	blackOutDate : function(e){
 			var selectedDates = self.$().datepicker('getDates');
+			self.selectedDates = selectedDates;
 			if(selectedDates.length > 2) {				
 			var maxDate = new Date(Math.max.apply(null,selectedDates));
 			var minDate = new Date(Math.min.apply(null,selectedDates));
+			self.startDate = minDate;
+			self.endDate = maxDate;
 
 			selectedDates.forEach(
 				function(dateVal) {
 					var date = new Date(dateVal);
 					if((date < maxDate) && (date > minDate)) {
 						$('td').filter(function(){
-							if($(this).text() === date.getDate().toString() && ($(this).attr('class') === 'day active')){
-								console.log();
-								$(this).css({background : '#F00'});
+							if($(this).text() === date.getDate().toString()){
+								var dateClassNames = ($(this).attr('class')).split(' ');
+								console.log('classname length is '+dateClassNames.length);
+								if(dateClassNames.length >=2 ) {
+									if(dateClassNames.length == 2) {
+										if(dateClassNames[0]==='day' && dateClassNames[1]==='active') {
+											$(this).css({background : '#F00'});
+										}
+									}
+									else {
+										if(dateClassNames[0]==='day' && dateClassNames[1]==='active') {
+											$(this).css({background : '#F00'});
+										}	
+									}
+								}
 							}
 						});
 						//$("td:contains('"+date.getDate()+"')").css({background : '#F00'});
@@ -37,7 +50,33 @@ App.DatePickerView = Em.View.extend({
 				}
 			);	
 			}
-		});
+		},
+
+		blackOutDateOnChangeMonth : function(e) {
+			self.selectedDates.forEach(
+					
+				function(dateVal) {
+					var date = new Date(dateVal);
+					if((date < self.endDate) && (date > self.startDate)) {
+						$('td').filter(function(){
+							if($(this).text() === date.getDate().toString()){
+								var dateClassNames = ($(this).attr('class')).split(' ');
+								console.log('classname length is '+dateClassNames.length);
+							}
+						});
+					}
+				}
+
+				);
+		},
+	didInsertElement : function (){
+		var options = {
+			multidate : true
+		};
+		self = this;
+		this.$().datepicker(options);
+		this.$().datepicker().on('changeMonth',self.blackOutDateOnChangeMonth);
+		this.$().datepicker().on('changeDate',self.blackOutDate);
 	},
 
 	beforeShowDay : function(date) {
