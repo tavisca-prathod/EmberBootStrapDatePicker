0e85428e238b42b02cfdb7acbcb343176c916b68
Prashant <prathod@tavisca.com>
2014-10-03 16:45:02 +0530

    Added the lib for datepicker using the jquery ui

--- starter-kit-1.7.0/js/app.js
+++ starter-kit-1.7.0/js/app.js
@@ -8,78 +8,28 @@ App.Router.map(function(){
 
 App.DatePickerView = Em.View.extend({
 	tagName : 'input',
-	self : null,
-	selectedDates : null,
+	self: this,
 	startDate : null,
 	endDate : null,
-	blackOutDate : function(e){
-			var selectedDates = self.$().datepicker('getDates');
-			self.selectedDates = selectedDates;
-			if(selectedDates.length > 2) {				
-			var maxDate = new Date(Math.max.apply(null,selectedDates));
-			var minDate = new Date(Math.min.apply(null,selectedDates));
-			self.startDate = minDate;
-			self.endDate = maxDate;
-
-			selectedDates.forEach(
-				function(dateVal) {
-					var date = new Date(dateVal);
-					if((date < maxDate) && (date > minDate)) {
-						$('td').filter(function(){
-							if($(this).text() === date.getDate().toString()){
-								var dateClassNames = ($(this).attr('class')).split(' ');
-								console.log('classname length is '+dateClassNames.length);
-								if(dateClassNames.length >=2 ) {
-									if(dateClassNames.length == 2) {
-										if(dateClassNames[0]==='day' && dateClassNames[1]==='active') {
-											$(this).css({background : '#F00'});
-										}
-									}
-									else {
-										if(dateClassNames[0]==='day' && dateClassNames[1]==='active') {
-											$(this).css({background : '#F00'});
-										}	
-									}
-								}
-							}
-						});
-						//$("td:contains('"+date.getDate()+"')").css({background : '#F00'});
-						//console.log($('td[value="'+date.getDate()+'"]'));
-						//self.$(jqElement).className += 'selected-excluded-date';
-					}
-				}
-			);	
-			}
-		},
-
-		blackOutDateOnChangeMonth : function(e) {
-			if(self.selectedDates!=null)
-			self.selectedDates.forEach(
-				function(dateVal) {
-					var date = new Date(dateVal);
-					if((date < self.endDate) && (date > self.startDate)) {
-						$('td').filter(function(){
-							if($(this).text() === date.getDate().toString()){
-								var dateClassNames = ($(this).attr('class')).split(' ');
-								console.log('classname length is '+dateClassNames.length);
-							}
-						});
-					}
-				}
-
-				);
-		},
 	didInsertElement : function (){
-		var options = {
-			multidate : true
-		};
-		self = this;
-		this.$().datepicker(options);
-		this.$().datepicker().on('changeMonth',self.blackOutDateOnChangeMonth);
-		this.$().datepicker().on('changeDate',self.blackOutDate);
-	},
+		var opts = {
+			onSelect : function(dateText,inst){
+				inst.inline = true;
+				var dateRangeValue = inst.input.val();
+				if(dateRangeValue !== null && dateRangeValue !== undefined && (dateRangeValue.split("-").length-1)>=1) {
+					if((dateRangeValue.split("-").length-1)==1) {
+						self.startDate = dateRangeValue.split("-")[0];
+						self.endDate = dateRangeValue.split("-")[1];
+					}
+					else if((dateRangeValue.split("-").length-1)>3){
+						self.startDate = dateRangeValue.split("-")[0];
+						self.endDate = dateRangeValue.split("-")[1];
+					}
+				}
 
-	beforeShowDay : function(date) {
-		return {classes : 'selected-excluded-date'};
+
+			}
+		};
+		this.$().daterange(opts);
 	}
 });
