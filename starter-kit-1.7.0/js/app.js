618bcbac23e92c195d66e06601aefd9189e20829
unknown <prashrathod7@gmail.com>
2014-10-06 00:05:35 +0530

    Changed Code for looping of date array

--- starter-kit-1.7.0/js/app.js
+++ starter-kit-1.7.0/js/app.js
@@ -18,16 +18,34 @@ App.DatePickerView = Em.View.extend({
 				var dateRangeValue = inst.input.val();
 				if(dateRangeValue !== null && dateRangeValue !== undefined && (dateRangeValue.split("-").length-1)>=1) {
 					if((dateRangeValue.split("-").length-1)==1) {
-						self.startDate = dateRangeValue.split("-")[0];
-						self.endDate = dateRangeValue.split("-")[1];
+						var startDate = new Date(dateRangeValue.split("-")[0]);
+						self.startDate = new Date(startDate.getYear(),startDate.getMonth(),startDate.getDate());
+						var endDate = new Date(dateRangeValue.split("-")[1]);
+						self.endDate = new Date(endDate.getYear(),endDate.getMonth(),endDate.getDate());
 					}
 					else if((dateRangeValue.split("-").length-1)>3){
-						self.startDate = dateRangeValue.split("-")[0];
-						self.endDate = dateRangeValue.split("-")[1];
+						var startDate = new Date(dateRangeValue.split("-")[0]);
+						self.startDate = new Date(startDate.getYear(),startDate.getMonth(),startDate.getDate());
+						var endDate = new Date(dateRangeValue.split("-")[1]);
+						self.endDate = new Date(endDate.getYear(),endDate.getMonth(),endDate.getDate());
 					}
 				}
 
-
+				//console.log((self.$('td[data-month="'+self.startDate.getMonth()+'"[data-year="'+self.startDate.getYear()+'"]')).length);
+				if(self.startDate !== undefined && self.endDate !== undefined) {
+					var count = 0;
+					console.log((self.$('td[data-month="'+self.startDate.getMonth()+'"]'+'[data-year="'+self.startDate.getFullYear()+'"]')).length);
+					console.log(self.startDate);
+					console.log(self.endDate);
+					console.log(self.startDate.getDate());
+					console.log(self.startDate.setDate(self.endDate.getDate()+1));
+					for(self.startDate;self.startDate <= self.endDate; self.startDate.setDate(self.endDate.getDate()+1)){
+						//$("td[data-month='9'][data-year='2014']").children("a:contains(9)");
+						console.log("running for "+count);
+						if(count >10)
+							break;
+					}
+				}
 			}
 		};
 		this.$().daterange(opts);
