01f806042ef8fe0955f2155b3ed8a39745860ed4
Prashant <prathod@tavisca.com>
2014-10-08 16:40:12 +0530

    Modeified the code according to the requirements

--- starter-kit-1.7.0/js/app.js
+++ starter-kit-1.7.0/js/app.js
@@ -12,49 +12,47 @@ App.DatePickerView = Em.View.extend({
 	endDate : null,
 	disableDatesArray : ['10/14/2014','10/17/2014'],
 	days : ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],
-	daysMin : [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa" ],
-	disableDaysArray : ['Monday','Tu'],
+	daysMin : [ "Su", "Mo", "Tu", "We", "Th", "Fr", "Sat" ],
+	disableDaysArray : ['Tu'],
 	dateRange : 3,
-	dateFormat : 'dd-mm-yy',
+	dateFormat : 'mm-dd-yy',
+	rangeSeparator : '-',
+	disabledDates : [],
+	onRangeSelect : function(dataText,inst){
+				var self = this;
+				inst.inline = true;
+				var dateRangeValue = inst.input.val();
+				var datesSeparator = ' '+self.get('rangeSeparator')+' ';
+				if(dateRangeValue !== null && dateRangeValue !== undefined && (dateRangeValue.split(datesSeparator).length-1)>=1) {
+						var startDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split(datesSeparator)[0]));
+						self.set('startDate',startDate);
+						var endDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split(datesSeparator)[1]).trim());
+						self.set('endDate',endDate);
+				}
+			},
 	didInsertElement : function (){
 		var self = this;
 		var opts = {
 			dateFormat : self.get('dateFormat'),
 			maxDate : new Date('11/6/2014'),
 			minDate : new Date('10/6/2014'),
-			onSelect : function(dataText,inst){
-				inst.inline = true;
-				var dateRangeValue = inst.input.val();
-			//	self.getEndDateForRangeSpecified(dateRangeValue);
-				if(dateRangeValue !== null && dateRangeValue !== undefined && (dateRangeValue.split("-").length-1)>=1) {
-					if(((dateRangeValue.split("-").length-1)==1)) {
-						var startDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split("-")[0]));
-						self.set('startDate',startDate);
-						var endDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split("-")[1]));
-						self.set('endDate',endDate);
-						//self.getEndDateForRangeSpecified(endDate);
-					}
-					else if(((dateRangeValue.split("-").length-1) > 3)) {
-						var startDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split(" - ")[0]));
-						self.set('startDate',startDate);
-						var endDate = $.datepicker.parseDate(self.get('dateFormat'),(dateRangeValue.split(" - ")[1]));
-						self.set('endDate',endDate);
-					}
-				}
-			},
+			dayNames : self.get('days'),
+			dayNamesMin : self.get('daysMin'),
+			onSelect : $.proxy(self.onRangeSelect,self),
 			beforeShowDay : function(date){
+
 				//format of formattedDate is in yy-mm-dd for comparison
 				var formattedDate = new Date(date.getFullYear(),date.getMonth(),date.getDate());
 				/* Conditions to make date disable are.
 					1.Date should be in disableDatesArray in format mm/dd/yy
 					2.Day should be in the disableDaysArray in format Monday or Mo 
 				*/
-				if(self.isInArray(self.convertDateToString(formattedDate),self.get('disableDatesArray')) || self.isInArray((self.get('days'))[formattedDate.getDay()],self.get('disableDaysArray')) || self.isInArray((self.get('daysMin'))[formattedDate.getDay()],self.get('disableDaysArray'))) {
+				if(self.isInDisableDatesArray(formattedDate) || self.isInDisableDaysArray(formattedDate)) {
 					return [false, '', ''];	
 				}
 
 				if(self.get('startDate') !== null && self.get('endDate')!==null) {
-					if(formattedDate >= self.get('startDate') && formattedDate <= self.get('endDate') && !self.isInArray(self.convertDateToString(formattedDate),self.get('disableDatesArray')) && !self.isInArray((self.get('days'))[formattedDate.getDay()],self.get('disableDaysArray')) && !self.isInArray((self.get('daysMin'))[formattedDate.getDay()],self.get('disableDaysArray')))
+					if(formattedDate >= self.get('startDate') && formattedDate <= self.get('endDate') && !self.isInDisableDatesArray(formattedDate) && !self.isInDisableDaysArray(formattedDate))
 						return [true,'selected-excluded-date',''];	
 					else
 						return [true, '', ''];		
@@ -70,6 +68,14 @@ App.DatePickerView = Em.View.extend({
 	  e.g -> January => 0,
 	  		 February => 1	
 	*/
+
+	isInDisableDatesArray : function (date) {
+		return this.isInArray(this.convertDateToString(date),this.get('disableDatesArray'));
+	},
+
+	isInDisableDaysArray : function(date) {
+		return (this.isInArray((this.get('days'))[date.getDay()],this.get('disableDaysArray')) || this.isInArray((this.get('daysMin'))[date.getDay()],this.get('disableDaysArray')));
+	},
 
 	convertDateToString : function(date) {
 		return (date.getMonth()+1)+'/'+date.getDate()+'/'+date.getFullYear();
